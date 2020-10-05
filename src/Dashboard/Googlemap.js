52aa92f6d705a32dfb84bdf169e335d35b6534fa
sayanmondal2098@gmail.com <sayanmondal2098@gmail.com>
2020-10-05 22:49:44 +0530

    remove sh files

--- src/Dashboard/Googlemap.js
+++ src/Dashboard/Googlemap.js
@@ -1,44 +1,67 @@
 import React, { Component } from 'react'
 import GoogleMapReact from 'google-map-react';
-import {
-    withScriptjs,
-    withGoogleMap,
-    GoogleMap,
-    Marker,
-    Polyline
-  } from "react-google-maps";
+// import {
+//     withScriptjs,
+//     withGoogleMap,
+//     GoogleMap,
+//     Marker,
+//     Polyline
+//   } from "react-google-maps";
 
   import { GOOGLE_MAP_API_KEY } from "../Keys";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-export default class Googlemap extends Component {
-    static defaultProps = {
-        center: {
-            lat: 59.95,
-            lng: 30.33
-        },
-        zoom: 15
-    };
-    render() {
-        return (
-            // Important! Always set the container height explicitly
-            <div style={{ height: '100vh', width: '100%' }}>
-                <GoogleMap defaultZoom={7} defaultCenter={{ lat: -34.897, lng: 151.144 }}>
-                    <Polyline path={[{ lat: -34.397, lng: 150.644 }, { lat: -35.397, lng: 151.644 }]} />
-                </GoogleMap>
-                <GoogleMapReact
-                    bootstrapURLKeys={{ key: GOOGLE_MAP_API_KEY }}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
-                >
-                    <AnyReactComponent
-                        lat={59.955413}
-                        lng={30.337844}
-                        text="My Car"
-                    />
-                </GoogleMapReact>
+import {Map, InfoWindow, Polyline,Marker, GoogleApiWrapper} from 'google-maps-react';
+ 
+export class Googlemap extends Component {
+    state = {
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {},
+      };
+     
+      onMarkerClick = (props, marker, e) =>
+        this.setState({
+          selectedPlace: props,
+          activeMarker: marker,
+          showingInfoWindow: true
+        });
+     
+      onMapClicked = (props) => {
+        if (this.state.showingInfoWindow) {
+          this.setState({
+            showingInfoWindow: false,
+            activeMarker: null
+          })
+        }
+      };
+     
+  render() {
+    const triangleCoords = [
+        {lat: 25.774, lng: -80.190},
+        {lat: 18.466, lng: -66.118},
+        {lat: 32.321, lng: -64.757},
+        {lat: 25.774, lng: -80.190}
+      ];
+      return(
+        <Map google={this.props.google}
+          onClick={this.onMapClicked}>
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
             </div>
-        );
-    }
+        </InfoWindow>
+      </Map>
+    );
+  }
 }
+ 
+export default GoogleApiWrapper({
+  apiKey: (GOOGLE_MAP_API_KEY)
+})(Googlemap)
